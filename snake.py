e0764a155d88ebe8b1cbb5390f8e9ddac53b3f5c
João Victor <joaovfreitas00@gmail.com>
2026-04-24 12:23:56 -0300

    Parte 2 - Utilização do Pygame para renderização

--- snake.py
+++ snake.py
@@ -1,62 +1,105 @@
 import os
-import keyboard
-import time
+import pygame
 import random
+from enum import Enum
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+TILE_SIZE = 32
+GRID_WIDTH = WINDOW_WIDTH // TILE_SIZE
+GRID_HEIGHT = WINDOW_HEIGHT // TILE_SIZE
+
+class SpriteDirection(Enum):
+    HEAD_UP = 1
+    HEAD_DOWN = 2
+    HEAD_LEFT = 3
+    HEAD_RIGHT = 4
+    TAIL_UP = 5
+    TAIL_DOWN = 6
+    TAIL_LEFT = 7
+    TAIL_RIGHT = 8
+    BODY_VERTICAL = 9
+    BODY_HORIZONTAL = 10
+    BODY_TOP_LEFT = 11
+    BODY_TOP_RIGHT = 12
+    BODY_BOTTOM_LEFT = 13
+    BODY_BOTTOM_RIGHT = 14
 
 class io_handler:
-    
-    x_size: int
-    y_size: int
-    game_speed: float
-    last_input: str
-    matrix = []
-    snake: list[tuple[int, int]]
-    fruit: tuple[int, int]
-
-    def __init__(self, dim, speed):
-        self.x_size = dim[0]
-        self.y_size = dim[1]
-        
-        self.game_speed = speed
+    def __init__(self):
         self.last_input = 'w'
 
-        self.snake = [(self.x_size//2, self.y_size//2)]
-        self.fruit = (random.randint(0,self.x_size),random.randint(0,self.y_size))
+        start_x = (GRID_WIDTH // 2) * TILE_SIZE
+        start_y = (GRID_HEIGHT // 2) * TILE_SIZE
+        self.snake = [(start_x, start_y), (start_x - TILE_SIZE, start_y)]
 
-        if self.fruit == self.snake[0]:
-            self.fruit = (self.fruit[0]-1,self.fruit[1]-1)
+        self.gerar_nova_fruta()
 
-        for i in range (self.y_size): 
-            self.matrix.append([0]*self.x_size)
+    def aligned(self, pos):
+        return (pos[0] // TILE_SIZE, pos[1] // TILE_SIZE)
 
     def move_snake(self):
-        if len(self.snake) == 0: 
+        if len(self.snake) == 0:
             return
         
-        hx, hy = self.snake[0] 
+        hx, hy = self.snake[0]
 
         if self.last_input == 'w':
-            nova_cabeca = (hx, self.y_size - 1 if hy == 0 else hy - 1)
+            nova_cabeca = (hx, WINDOW_HEIGHT - TILE_SIZE if hy <= 0 else hy - TILE_SIZE)
         elif self.last_input == 'a':
-            nova_cabeca = (self.x_size - 1 if hx == 0 else hx - 1, hy)
+            nova_cabeca = (WINDOW_WIDTH - TILE_SIZE if hx <= 0 else hx - TILE_SIZE, hy)
         elif self.last_input == 's':
-            nova_cabeca = (hx, 0 if hy == self.y_size - 1 else hy + 1)
+            nova_cabeca = (hx, 0 if hy >= WINDOW_HEIGHT - TILE_SIZE else hy + TILE_SIZE)
         elif self.last_input == 'd':
-            nova_cabeca = (0 if hx == self.x_size - 1 else hx + 1, hy)
+            nova_cabeca = (0 if hx >= WINDOW_WIDTH - TILE_SIZE else hx + TILE_SIZE, hy)
         else:
             nova_cabeca = (hx, hy)
 
         self.snake.insert(0, nova_cabeca)
 
-        if nova_cabeca == self.fruit:
+        if self.aligned(nova_cabeca) == self.aligned(self.fruit):
             self.gerar_nova_fruta()
         else:
             self.snake.pop()
 
+    def change_direction(self, direction):
+        if (self.last_input == 'w' and direction != 's') or \
+           (self.last_input == 's' and direction != 'w') or \
+           (self.last_input == 'a' and direction != 'd') or \
+           (self.last_input == 'd' and direction != 'a'):
+            self.last_input = direction
+            
+    def direction_between(self, pos1, pos2):
+        x1, y1 = pos1
+        x2, y2 = pos2
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        if abs(dx) > WINDOW_WIDTH // 2:
+            dx = -dx
+
+        if abs(dy) > WINDOW_HEIGHT // 2:
+            dy = -dy
+
+        if dx > 0:
+            return 'd'
+        elif dx < 0:
+            return 'a'
+        elif dy > 0:
+            return 's'
+        else:
+            return 'w'
+
+    def is_pair(self,a,b,d1,d2):
+        return (d1 == a and d2 == b) or (d1 == b and d2 == a)
+
     def gerar_nova_fruta(self):
         while True:
-            nova_fruta = (random.randint(0, self.x_size - 1), random.randint(0, self.y_size - 1))
+            nova_fruta = (
+                random.randrange(0, WINDOW_WIDTH, TILE_SIZE),
+                random.randrange(0, WINDOW_HEIGHT, TILE_SIZE),
+            )
             
             if nova_fruta not in self.snake:
                 self.fruit = nova_fruta
@@ -66,78 +109,115 @@ class io_handler:
         head = self.snake[0]
         if head in self.snake[1:]:
             self.last_input = 'end'
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
             print("Game Over! A cobra colidiu consigo mesma.")
 
-    def record_inputs(self):
-        keyboard.add_hotkey('w', lambda: setattr(self, "last_input", 'w'))
-        keyboard.add_hotkey('a', lambda: setattr(self, "last_input", 'a'))
-        keyboard.add_hotkey('s', lambda: setattr(self, "last_input", 's'))
-        keyboard.add_hotkey('d', lambda: setattr(self, "last_input", 'd'))
-        keyboard.add_hotkey('esc', lambda: setattr(self, "last_input", 'end'))
+    def get_sprite(self, i):
+        length = len(self.snake)
 
-    def display(self):
-        # Limpar a matriz
-        for i in range(self.y_size):
-            for j in range(self.x_size):
-                self.matrix[i][j] = 0
+        if i == 0:
+            dir = self.direction_between(self.snake[1], self.snake[0])
+            if dir == 'w': return SpriteDirection.HEAD_UP
+            elif dir == 'a': return SpriteDirection.HEAD_LEFT
+            elif dir == 's': return SpriteDirection.HEAD_DOWN
+            else: return SpriteDirection.HEAD_RIGHT
 
-        # Marca o corpo da cobra
-        for i in range(1, len(self.snake)):
-            x, y = self.snake[i]
-            if 0 <= x < self.x_size and 0 <= y < self.y_size:
-                self.matrix[y][x] = 1  # 1 = corpo
-        
-        # Marca a cabeça da cobra
-        if len(self.snake) > 0:
-            x, y = self.snake[0]
-            if 0 <= x < self.x_size and 0 <= y < self.y_size:
-                self.matrix[y][x] = 2  # 2 = cabeça
-        
-        # Marca a fruta
-        fx, fy = self.fruit
-        if 0 <= fx < self.x_size and 0 <= fy < self.y_size:
-            self.matrix[fy][fx] = 3  # 3 = fruta
+        if i == length - 1:
+            dir = self.direction_between(self.snake[length - 2], self.snake[length - 1])
+            if dir == 'w': return SpriteDirection.TAIL_UP
+            elif dir == 'a': return SpriteDirection.TAIL_LEFT
+            elif dir == 's': return SpriteDirection.TAIL_DOWN
+            else: return SpriteDirection.TAIL_RIGHT
 
-        def display_h_line(self):
-            print ('+', end='')
-            print ('--'* len(self.matrix[0]), end='')
-            print ('+')
-        
-        def display_content_line(line):
-            print ('|', end='')
-            for item in line: 
-                if item == 1:
-                    print ('[]', end='')
-                elif item == 2:
-                    print ('<>', end='')
-                elif item == 3:
-                    print ('()', end='')
-                else:
-                    print ('  ', end='')
+        prev = self.snake[i - 1]
+        curr = self.snake[i]
+        next = self.snake[i + 1]
 
-            print ('|')
+        d1 = self.direction_between(curr, prev)
+        d2 = self.direction_between(curr, next)
 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        display_h_line(self)
-        for line in self.matrix:
-            display_content_line(line)
-        display_h_line(self)
+        if self.is_pair('w', 's', d1, d2):
+            return SpriteDirection.BODY_VERTICAL
+        elif self.is_pair('a', 'd', d1, d2):
+            return SpriteDirection.BODY_HORIZONTAL
+        elif self.is_pair('w', 'd', d1, d2):
+            return SpriteDirection.BODY_TOP_RIGHT
+        elif self.is_pair('w', 'a', d1, d2):
+            return SpriteDirection.BODY_TOP_LEFT
+        elif self.is_pair('s', 'd', d1, d2):
+            return SpriteDirection.BODY_BOTTOM_RIGHT
+        else:
+            return SpriteDirection.BODY_BOTTOM_LEFT
+    
+    def ret_right_sprite(self, i):
+        sprite = self.get_sprite(i)
+        return SPRITES[sprite]
 
-### exemplo do uso da classe io_handler  
-instance = io_handler((10,15), 0.25)
 
 def game_loop():
-    instance.record_inputs()
-    while True:
-        instance.display()
-        print("mova com WASD, saia com esc. Ultimo botão:", end=' ')
-        ###adicione seu código para lidar com o jogo aqui
+    global SPRITES
+
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+
+    SPRITES = {
+        SpriteDirection.HEAD_UP: pygame.image.load('./Graphics/head_up.png').convert_alpha(),
+        SpriteDirection.HEAD_DOWN: pygame.image.load('./Graphics/head_down.png').convert_alpha(),
+        SpriteDirection.HEAD_LEFT: pygame.image.load('./Graphics/head_left.png').convert_alpha(),
+        SpriteDirection.HEAD_RIGHT: pygame.image.load('./Graphics/head_right.png').convert_alpha(),
+        SpriteDirection.TAIL_UP: pygame.image.load('./Graphics/tail_up.png').convert_alpha(),
+        SpriteDirection.TAIL_DOWN: pygame.image.load('./Graphics/tail_down.png').convert_alpha(),
+        SpriteDirection.TAIL_LEFT: pygame.image.load('./Graphics/tail_left.png').convert_alpha(),
+        SpriteDirection.TAIL_RIGHT: pygame.image.load('./Graphics/tail_right.png').convert_alpha(),
+        SpriteDirection.BODY_VERTICAL: pygame.image.load('./Graphics/body_vertical.png').convert_alpha(),
+        SpriteDirection.BODY_HORIZONTAL: pygame.image.load('./Graphics/body_horizontal.png').convert_alpha(),
+        SpriteDirection.BODY_TOP_LEFT: pygame.image.load('./Graphics/body_topleft.png').convert_alpha(),
+        SpriteDirection.BODY_TOP_RIGHT: pygame.image.load('./Graphics/body_topright.png').convert_alpha(),
+        SpriteDirection.BODY_BOTTOM_LEFT: pygame.image.load('./Graphics/body_bottomleft.png').convert_alpha(),
+        SpriteDirection.BODY_BOTTOM_RIGHT: pygame.image.load('./Graphics/body_bottomright.png').convert_alpha(),
+    }
+
+    food_img = pygame.image.load('./Graphics/apple.png').convert_alpha()
+
+    instance = io_handler()
+    running = True
+
+    while running:
+        screen.fill((0, 0, 0))
+
+        # desenhar comida
+        screen.blit(food_img, instance.fruit)
+
+        # desenhar cobra
+        for i in range(len(instance.snake)):
+            x, y = instance.snake[i]
+            sprite = instance.ret_right_sprite(i)
+            screen.blit(sprite, (x, y))
+
         instance.move_snake()
         instance.check_game_over()
-        print(instance.last_input)
-        if(instance.last_input == 'end'):
-            exit()
-        time.sleep(instance.game_speed)
 
-if __name__ == '__main__':
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    instance.change_direction('w')
+                elif event.key == pygame.K_a:
+                    instance.change_direction('a')
+                elif event.key == pygame.K_s:
+                    instance.change_direction('s')
+                elif event.key == pygame.K_d:
+                    instance.change_direction('d')
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
+
+        pygame.display.flip()
+        clock.tick(10) #velocidade da cobra
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
     game_loop()
--- test_snake.py
+++ test_snake.py
@@ -1,47 +1,39 @@
-from snake_ref import io_handler
+from snake import io_handler
+from snake import WINDOW_HEIGHT,WINDOW_WIDTH,TILE_SIZE, GRID_WIDTH,GRID_HEIGHT
 import pytest
 
 @pytest.fixture
 def handler():
-    return io_handler((10,15), 0.5)
+    return io_handler()
 
 def test_initialization(handler):
-    assert handler.x_size == 10
-    assert handler.y_size == 15
-    assert handler.game_speed == 0.5
+    assert handler.snake == [((GRID_WIDTH // 2) * TILE_SIZE, (GRID_HEIGHT // 2) * TILE_SIZE), ((GRID_WIDTH // 2 - 1) * TILE_SIZE, (GRID_HEIGHT // 2) * TILE_SIZE)]  
     assert handler.last_input == 'w'
 
 def test_move(handler):
-    handler.snake = [(5,5)]
+    snake = handler.snake.copy()
     handler.last_input = 'w'
     handler.move_snake()
-    assert handler.snake == [(5,4)]
+    assert handler.snake == [(snake[0][0], snake[0][1] - TILE_SIZE), snake[0]]
 
 def test_move_cross_boundaries(handler):
-    handler.snake = [(handler.x_size,14)]
+    handler.snake = [(WINDOW_WIDTH//2,WINDOW_HEIGHT), (WINDOW_WIDTH//2,WINDOW_HEIGHT - TILE_SIZE)]
     handler.last_input = 's'
     handler.move_snake()
-    assert handler.snake == [(handler.x_size,0)]
+    assert handler.snake == [(WINDOW_WIDTH//2,0), (WINDOW_WIDTH//2,WINDOW_HEIGHT)]
     
     
 def test_snake_growth(handler):
-    handler.snake = [(5,5)]
-    handler.fruit = (5,4)
-    handler.last_input = 'w'
+    handler.snake = [(WINDOW_WIDTH//2,WINDOW_HEIGHT//2), (WINDOW_WIDTH//2 - TILE_SIZE,WINDOW_HEIGHT//2)]
+    handler.fruit = (WINDOW_WIDTH//2 + TILE_SIZE,WINDOW_HEIGHT//2)
+    len_before = len(handler.snake)
+    handler.last_input = 'd'
     handler.move_snake()
-    assert handler.snake == [(5,4), (5,5)]
+    assert len(handler.snake) == len_before + 1
 
 def test_game_over(handler):
-    handler.snake = [(5,5), (5,4), (5,3)]
-    handler.last_input = 'w'
+    handler.snake = [(WINDOW_WIDTH//2,WINDOW_HEIGHT//2), (WINDOW_WIDTH//2 - TILE_SIZE,WINDOW_HEIGHT//2),(WINDOW_WIDTH//2 - 2*TILE_SIZE,WINDOW_HEIGHT//2)]
+    handler.last_input = 'a'
     handler.move_snake()
     handler.check_game_over()
-    assert handler.snake == [(5,4), (5,5), (5,4)]
-    assert handler.last_input == 'end'
-
-def test_score(handler):
-    handler.snake = [(5,5)]
-    handler.fruit = (5,4)
-    handler.last_input = 'w'
-    handler.move_snake()
-    assert len(handler.snake) == 2
+    assert len(handler.snake) != len(set(handler.snake)) # check for duplicates in snake body
